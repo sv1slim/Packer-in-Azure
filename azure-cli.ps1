4dae84a6daa58bfaefc9959aeeafc55b1cb2e2ac
Serge Voloshenko <serge.voloshenko@mhs.com>
2024-07-09 22:01:49 -0400

    New working Packer

--- azure-cli.ps1
+++ azure-cli.ps1
@@ -11,9 +11,9 @@ az group create -n myPackerResourceGroup -l eastus
 az group list --output table
 
 # Create a service principal and paste the IDs into ubuntu.json file 
-az ad sp create-for-rbac --role Contributor --scopes /subscriptions/f7806be4-19fa-45cf-b6e7-636c8ebd49d9 --query "{ client_id: appId, client_secret: password, tenant_id: tenant }"
-
-az role assignment list --subscription "f7806be4-19fa-45cf-b6e7-636c8ebd49d9" --assignee "c65add7d-b105-463f-bc42-d9a7fa0ff660"
+az ad sp create-for-rbac --role Contributor --scopes /subscriptions/"{subscription_id}" --query "{ client_id: appId, client_secret: password, tenant_id: tenant }"
+az role assignment list --scope /subscriptions/"{subscription_id}" --role "Contributor"
+az role assignment list --assignee "{client_id}"
 
 # Obtain your Azure subscription ID
 az account show --query "{ subscription_id: id }"
