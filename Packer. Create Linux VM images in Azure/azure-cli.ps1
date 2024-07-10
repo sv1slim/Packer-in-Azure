c6ec99d10a946d2d144022b3c737a9ff270e9b86
Serge Voloshenko <serge.voloshenko@mhs.com>
2024-07-10 01:04:41 -0400

    adding client_id so it can be queried

--- Packer. Create Linux VM images in Azure/azure-cli.ps1
+++ Packer. Create Linux VM images in Azure/azure-cli.ps1
@@ -13,7 +13,7 @@ az group list --output table
 # Create a service principal and paste the IDs into ubuntu.json file 
 az ad sp create-for-rbac --role Contributor --scopes /subscriptions/f7806be4-19fa-45cf-b6e7-636c8ebd49d9 --query "{ client_id: appId, client_secret: password, tenant_id: tenant }"
 az role assignment list --scope /subscriptions/"f7806be4-19fa-45cf-b6e7-636c8ebd49d9" --role "Contributor"
-az role assignment list --assignee "{client_id}"
+az role assignment list --assignee "3dbd575d-64a2-4775-9a2c-faedd0ca84fe" # --assignee = "{client_id}}""
 
 # Obtain your Azure subscription ID
 az account show --query "{ subscription_id: id }"
